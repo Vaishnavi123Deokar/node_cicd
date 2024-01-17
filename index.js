815a43b9190c2d75ebee5276c1bfd0402b9ed197
Vaishnavi123Deokar <vaishnavideokar8@gmail.com>
2024-01-17 18:05:18 +0530

    navin wala commit

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
 <<<<<<< HEAD
- res.send('hello Everyone');
-=======
- res.send('hello all');
+ res.send('hello dearrrr');
 >>>>>>> 1856774cf481d5989d1e0c51d1d4927cd52f4ad1
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
