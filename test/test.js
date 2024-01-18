9f1260e3ff80ca11cbfd820be2d0fbd6cc1f1c77
Vaishnavi123Deokar <149041783+Vaishnavi123Deokar@users.noreply.github.com>
2024-01-19 03:41:26 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,25 +1,36 @@
 var request = require('supertest');
+
 var app = require('../index.js');
-
+ 
 describe('GET /', function() {
-  it('responds with 404 "Page Not Found"', function(done) {
-    request(app)
-      .get('/nonexistentpage')
-      .expect(404)
-      .end(function(err, res) {
-        if (err) {
-          // If there's an error during the request, log it and fail the test
-          console.error(err);
-          return done(err);
-        }
 
-        // Check if the response body contains a specific error message for 404
-        if (!res.text.includes('Page Not Found')) {
-          return done(new Error('Expected "Page Not Found" in response body'));
+  it('respond with 404 page not found', function(done) {
+
+    request(app)
+
+      .get('/nonexistentpage')
+
+      .expect(404)
+
+      .end(function(err, res) {
+
+        if (err) {
+
+          // If there's an error, log it and pass it to the done callback
+
+          console.error(err);
+
+          return done(err);
+
         }
 
         // If everything is fine, invoke the done callback
+
         done();
+
       });
+
   });
+
 });
+
